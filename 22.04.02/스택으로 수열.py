a96e908a96664c1eb6cd069c6adb505cc1198a25
PrettyHan <imbel9830@gmail.com>
2022-04-03 16:18:49 +0900

    1st

---
+++ 22.04.02/스택으로 수열.py
@@ -0,0 +1,20 @@
+'''
+is_stack_sequence 함수를 작성하세요.
+'''
+
+def is_stack_sequence(nums) :
+    # 1을 push 무적권
+    stack = [1]
+    
+    current = 0
+    
+    for i in range(2, len(nums) +1) :
+        while len(stack) > 0 and stack[-1] == nums[current]:
+            stack.pop()
+            current += 1
+        if len(stack) == 0 or stack[-1] < nums[current]:
+            stack.append(i)
+        else :
+            return "No"
+
+    return "Yes"
